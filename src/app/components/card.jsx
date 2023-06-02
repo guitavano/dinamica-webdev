1601d93ddab6737484280cb65a2463f5fe2854a3
MatheusApRocky <matheus.ramos@rocky.ag>
2023-06-02 16:09:51 -0300

    fix cards

---
+++ src/app/components/card.jsx
@@ -0,0 +1,17 @@
+function Card () {
+    return(
+        <div class="w-full bg-[#FFCC00] h-full mx-4 my-8 flex flex-col justify-center items-center p-2 rounded-md" >
+            <image class="w-[100px] h-[100px] rounded-full bg-white">
+
+            </image>
+            <h2 class="font text-center my-3 text-xl">
+            Celebração do Dia dos Clientes!
+            </h2>
+            <p class="text-center text-[#63038C]">
+            Descontos especiais e brindes surpresa para homenagear você, nosso cliente querido. Venha comemorar conosco!
+            </p>
+        </div>
+    )
+}
+
+export default Card
